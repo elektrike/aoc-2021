a532eba41302f42556eb333da4c800026a4a147c
elektrike <solargraphs@gmail.com>
2021-12-02 22:24:10 +0200

    Day01 - remove wrapping

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,8 @@
 fun main() {
-    fun calculateIncreases(input: List<String>, windowSize: Int): Int {
-        return input.asSequence()
-            .map { it.toInt() }
-            .windowed(windowSize, 1)
-            .count { it.first() < it.last() }
-    }
+    fun calculateIncreases(input: List<String>, windowSize: Int): Int =
+        input.map { it.toInt() }
+             .windowed(windowSize, 1)
+             .count { it.first() < it.last() }
 
     fun part1(input: List<String>): Int = calculateIncreases(input, 2)
     fun part2(input: List<String>): Int = calculateIncreases(input, 4)
