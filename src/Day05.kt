977cbaf3156b452cc85dd8e3db7c40913162b231
elektrike <solargraphs@gmail.com>
2021-12-05 22:18:29 +0200

    Day05

---
+++ src/Day05.kt
@@ -0,0 +1,47 @@
+fun main() {
+    data class Point(val x: Int, val y: Int)
+
+    data class Line(val from: Point, val to: Point) {
+        fun isHorizontal(): Boolean = from.x == to.x
+        fun isVertical(): Boolean = from.y == to.y
+        fun isDiagonal(): Boolean = !isHorizontal() && !isVertical()
+    }
+
+    fun String.toPoint(): Point {
+        val xy = split(",").map { it.toInt() }
+        return Point(xy.first(), xy.last())
+    }
+
+    fun List<String>.toLines(): List<Line> =
+        map { it.split(" -> ") }
+            .map { Line(it.first().toPoint(), it.last().toPoint()) }
+
+    fun collectRange(from: Int, to: Int): List<Int> =
+        if (from < to)
+            (from..to).toList()
+        else
+            (from downTo to).toList()
+
+    fun drawLines(lines: List<Line>): List<Point> =
+        lines.flatMap {
+            val xs = collectRange(it.from.x, it.to.x)
+            val ys = collectRange(it.from.y, it.to.y)
+
+            if (it.isHorizontal())
+                ys.map { y -> Point(xs.first(), y) }.toList()
+            else if (it.isVertical())
+                xs.map { x -> Point(x, ys.first()) }.toList()
+            else
+                List(xs.size) { i -> Point(xs[i], ys[i]) }
+        }
+
+    fun countOverlappingPoints(lines: List<Line>): Int =
+        drawLines(lines)
+            .groupingBy { it }
+            .eachCount()
+            .filter { it.value > 1 }
+            .size
+
+    fun part1(input: List<String>): Int = countOverlappingPoints(input.toLines().filter { !it.isDiagonal() })
+    fun part2(input: List<String>): Int = countOverlappingPoints(input.toLines())
+}
